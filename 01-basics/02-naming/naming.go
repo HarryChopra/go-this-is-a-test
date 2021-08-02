96e76fe7d3ba1aa96010abbe78dcf81154cb9414
Harry Chopra <harrychopra@gmail.com>
2021-08-02 14:14:37 +0100

    naming conventions

--- 01-basics/02-naming/naming.go
+++ 01-basics/02-naming/naming.go
@@ -1,1 +1,38 @@
 package naming
+
+import "fmt"
+
+type Dog struct {
+	Name string
+	Age  int
+}
+
+func (d Dog) Bark(muzzled bool) {
+	if muzzled {
+		fmt.Println("woof")
+	} else {
+		fmt.Println("WOOF!!")
+	}
+}
+
+func Speak(lang string) {
+	switch lang {
+	case "es":
+		fmt.Println("Hola")
+	default:
+		fmt.Println("Hello")
+	}
+}
+
+func Color(name string) string {
+	switch name {
+	case "blue":
+		return "#0000ff"
+	case "green":
+		return "#00ff00"
+	case "red":
+		return "#ff0000"
+	default:
+		return "#000000"
+	}
+}
--- 01-basics/02-naming/naming_test.go
+++ 01-basics/02-naming/naming_test.go
@@ -1,1 +1,24 @@
 package naming
+
+import "testing"
+
+// Type_usecase
+func TestDog_puppy(t *testing.T) {}
+
+// Type_Method_usecase
+func TestDog_Bark_muzzled(t *testing.T) {}
+
+func TestDog_Bark_unmuzzled(t *testing.T) {}
+
+// Function_usecase
+func TestSpeak_spanish(t *testing.T) {}
+
+// Naming variables: arg, got, want ...
+func TestColor(t *testing.T) {
+	arg := "blue"
+	want := "#0000ff"
+	got := Color(arg)
+	if got != want {
+		t.Errorf("Color(%s) = %s; want %s", arg, got, want)
+	}
+}
