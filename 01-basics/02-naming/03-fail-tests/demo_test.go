b4800c129de8af601fc8102aa0479c8e0d975c51
Harry Chopra <harrychopra@gmail.com>
2021-08-02 16:18:13 +0100

    failing and logging

--- 01-basics/02-naming/03-fail-tests/demo_test.go
+++ 01-basics/02-naming/03-fail-tests/demo_test.go
@@ -1,1 +1,24 @@
 package demo
+
+import "testing"
+
+func TestFuncA1(t *testing.T) {
+	t.Logf("funcA error description")
+	t.Fail()
+	t.Log("continuing other tests..")
+}
+func TestFuncA2(t *testing.T) {
+	t.Errorf("funcA error description") // Errorf = Fail + Logf
+	t.Log("continuing other tests..")
+}
+
+func TestFuncB1(t *testing.T) {
+	t.Logf("funcB error description")
+	t.FailNow()
+	t.Log("continuing other tests..") // This won't log as the test would stop immediately
+}
+
+func TestFuncB2(t *testing.T) {
+	t.Fatalf("funcB error description") // Fatalf = Logf + FailNow()
+	t.Log("continuing other tests..")   // This won't log
+}
